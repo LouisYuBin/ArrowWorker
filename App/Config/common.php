a50e6b99a28845ca0cd01ef65b794a3658a08e78
louis <1210077683@qq.com>
2017-02-22 16:34:03 +0800

    update configuration

--- App/Config/common.php
+++ App/Config/common.php
@@ -37,6 +37,32 @@ return[
         'level'  => 30719
         //1:E_ERROR , 2:E_WARNING , 8:E_NOTICE , 2048:E_STRICT , 30719:all
     ],
+	'mongo' => [
+        'username' => 'userName',
+        'password' => '123456',
+        'host'     => '127.0.0.1',
+        'port'     => 27017,
+        'dbName'   => 'dbName',
+        /*
+		 * update and release comments if you have backup node
+        'secondary'=> [
+            [
+                'host' => '127.0.0.1',
+                'port' => 27017
+            ]
+        ],
+        */
+    ],
+    'rabbitmq' => [
+        'host'     => '127.0.0.1',
+        'port'     => '5672',
+        'login'    => 'usernanme',
+        'password' => 'password',
+        'vhost'    => 'vhost',
+        'exchange' => 'exchangeName',
+        'route'    => 'routeName',
+        'queue'    => 'queueName'
+    ],
     'view' => [
   	'driver' => 'Smarty',
 	'tplExt' => '.tpl'
