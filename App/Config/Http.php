ae99df7e1f6be7bdefec77f6ace13c5c4ed44546
louis <louis531029167@163.com>
2019-02-20 11:43:55 +0800

    add optimize swoole http config

--- App/Config/Http.php
+++ App/Config/Http.php
@@ -5,8 +5,13 @@ return [
     'port'      => 9502,
     //number of worker process
     'workerNum' => 8,
+    //react thread number
+    'reactor_num' => 4,
     //size of request queue
-    'backlog'   => 2000,
+    'backlog'   => 20000,
+    'pipe_buffer_size' => 1024*1024*100,
+    'socket_buffer_size' =>  1024*1024*100,
+    'max_coroutine' => 5000,
     //max post data length
     'maxContentLength' => 20889600,
     //is enable request for static file
