c99c8f113ee9a906e22ae4b1ea80a45dbe85d2db
Your Name <you@example.com>
2019-06-21 20:02:19 +0800

    change app type

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 use ArrowWorker\ArrowWorker as arrow;
 define('APP_PATH',__DIR__.'/App');
 //APP_TYPE要启动的应用数组，可选值： 1、worker（工作进程）  2、server（服务）
-//define('APP_TYPE',['worker','server']);
+define('APP_TYPE',['worker','server']);
 //define('APP_TYPE',['server']);
-define('APP_TYPE',['worker']);
+//define('APP_TYPE',['worker']);
 
 //开发调试模式
 define('DEBUG', false);
