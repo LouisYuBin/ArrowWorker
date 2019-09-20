038053148cb2bb3d823767b143d735a560ef45ca
louis <louis531029167@163.com>
2019-09-20 17:06:58 +0800

    finish redis poole

---
+++ ArrowWorker/Driver/Pool.interface.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * By yubin at 2019-09-20 16:58.
+ */
+
+namespace ArrowWorker\Driver;
+
+
+/**
+ * Interface Pool
+ * @package ArrowWorker\Driver
+ */
+interface Pool
+{
+
+    /**
+     * @param int
+     */
+    const DEFAULT_POOL_SIZE = 10;
+
+    /**
+     * @param array $appConfig
+     * @return mixed
+     */
+    public static function Init( array $appConfig) : void ;
+
+    /**
+     * @param string $alias
+     * @return mixed
+     */
+    public static function GetConnection( $alias = 'default' );
+
+    /**
+     * @return void
+     */
+    public static function Release() : void ;
+}
