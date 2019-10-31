ec37ef9b4fd11b1d054b80c02adc0b198b27b030
louis <louis531029167@163.com>
2019-10-31 16:01:24 +0800

    handle cache operation error

--- ArrowWorker/Driver/Cache/Redis.class.php
+++ ArrowWorker/Driver/Cache/Redis.class.php
@@ -8,6 +8,7 @@
 namespace ArrowWorker\Driver\Cache;
 
 use ArrowWorker\Log;
+use function Couchbase\defaultDecoder;
 
 
 /**
@@ -70,7 +71,11 @@ class Redis implements Cache
         return true;
     }
 
-    public function Init(array $config)
+    /**
+     * @param array $config
+     * @return Redis
+     */
+    public function Init( array $config)
     {
         return new self($config);
     }
@@ -82,7 +87,7 @@ class Redis implements Cache
 	 */
 	public function Db(int $dbName) : bool
     {
-         $this->_conn->select( $dbName );
+         return $this->_exec('select','');
     }
 
 
@@ -94,23 +99,28 @@ class Redis implements Cache
 	 */
 	public function Set(string $key, string $val) : bool
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->set( $key, $val );
-        }
-        catch (\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('set', $key, $val );
+    }
+
+    /**
+     * @param $key
+     * @param $val
+     * @return bool
+     */
+    public function SetNx(string $key, string $val) : bool
+    {
+        return $this->_exec('setnx', $key, $val );
+    }
+
+    /**
+     * @param string $key
+     * @param string $val
+     * @param int $ttl
+     * @return bool
+     */
+    public function SetEx(string $key, string $val, int $ttl) : bool
+    {
+        return $this->_exec('setex', $key, $val, $ttl );
     }
 
 
@@ -121,32 +131,47 @@ class Redis implements Cache
 	 */
 	public function Get(string $key)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->get($key);
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('get', $key);
     }
 
-    private function _handleException(\RedisException $exception, string $function)
+    /**
+     * @param string $key
+     * @param string $value
+     * @return mixed
+     */
+    public function Append( string $key, string $value)
     {
-        Log::Warning(__CLASS__.'::'.$function." failed, ".$exception->getMessage(), self::LOG_NAME);
-        if( 0==$exception->getCode() )
-        {
-            $this->InitConnection();
-        }
+        return $this->_exec('append', $key, $value);
+    }
+
+    /**
+     * @param int $option
+     * @return mixed
+     */
+    public function Multi( int $option=\Redis::MULTI)
+    {
+        return $this->_exec('multi', $option);
+    }
+
+    /**
+     * Watches a key for modifications by another client. If the key is modified between WATCH and EXEC,
+     * the MULTI/EXEC transaction will fail (return FALSE). unwatch cancels all the watching of all keys by this client.
+     * @param string | array $key: a list of keys
+     * @return void
+     * @link    https://redis.io/commands/watch
+     * @example
+     * <pre>
+     * $redis->watch('x');
+     * // long code here during the execution of which other clients could well modify `x`
+     * $ret = $redis->multi()
+     *          ->incr('x')
+     *          ->exec();
+     * // $ret = FALSE if x has been modified between the call to WATCH and the call to EXEC.
+     * </pre>
+     */
+    public function Watch(string $key)
+    {
+        return $this->_exec('watch', $key);
     }
 
 
@@ -154,28 +179,12 @@ class Redis implements Cache
 	 * Lpush : Adds the string values to the head (left) of the list. Creates the list if the key didn't exist.
      * If the key exists and is not a list, FALSE is returned
 	 * @param string $queue
-	 * @param mixed $val
+	 * @param string $val
 	 * @return int|false
 	 */
 	public function LPush(string $queue, string $val)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->lPush( $queue, $val );
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('lPush',$queue, $val);
     }
 
 
@@ -183,28 +192,12 @@ class Redis implements Cache
 	 * Rpush : Adds the string values to the tail (right) of the list. Creates the list if the key didn't exist.
      * If the key exists and is not a list, FALSE is returned.
 	 * @param string $queue
-	 * @param mixed $val
+	 * @param string $val
 	 * @return int|false
 	 */
 	public function RPush(string $queue, string $val)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->rPush( $queue, $val );
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('rPush', $queue, $val);
     }
 
 
@@ -216,23 +209,7 @@ class Redis implements Cache
 	 */
 	public function RPop(string $queue)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->rPop( $queue);
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('rPop', $queue);
     }
 
 	/**
@@ -243,23 +220,7 @@ class Redis implements Cache
 	 */
 	public function LPop(string $queue)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->lPop( $queue);
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('lPop', $queue);
     }
 
 
@@ -274,25 +235,9 @@ class Redis implements Cache
 	 * @return array
 	 *  Return value ：ARRAY array('listName', 'element')
 	 */
-	public function BrPop(int $timeout, string ...$queue )
+	public function BrPop( string $queue, int $timeout )
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->brPop ( $queue, $timeout );
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return [];
-        }
-        return $result;
+        return $this->_exec('brPop', $queue, $timeout);
     }
 
 	/**
@@ -306,25 +251,10 @@ class Redis implements Cache
 	 * @return mixed
 	 * 	  ARRAY array('listName', 'element')
 	 */
-	public function BlPop(int $timeout, string ...$queue)
+	public function BlPop(string $queue, int $timeout)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->blPop ( $queue, $timeout );
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return [];
-        }
-        return $result;
+        return $this->_exec('blPop', $queue, $timeout);
+
     }
 
 	/**
@@ -337,23 +267,31 @@ class Redis implements Cache
 	 */
 	public function HSet(string $key, string $hashKey, string $value)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->Hset ( $key, $hashKey, $value);
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('hSet', $hashKey, $value);
+    }
+
+    /**
+     * @param string $key
+     * @param string $hashKey
+     * @param string $value
+     * @return mixed
+     */
+    public function HSetNx( string $key, string $hashKey, string $value)
+    {
+        return $this->_exec('hSetNx', $hashKey, $value);
+    }
+
+    /**
+     * Hset hash table 写入
+     * @param string $key
+     * @param string $hashKey
+     * @param string $value
+     * @return mixed
+     *       LONG 1 if value didn't exist and was added successfully, 0 if the value was already present and was replaced, FALSE if there was an error.
+     */
+    public function HmSet(string $key, string $hashKey, string $value)
+    {
+        return $this->_exec('hmSet', $hashKey, $value);
     }
 
 	/**
@@ -365,23 +303,7 @@ class Redis implements Cache
 	 */
 	public function HGet(string $key, string $hashKey)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->hGet ( $key, $hashKey );
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('hGet', $key, $hashKey);
     }
 
 
@@ -393,25 +315,113 @@ class Redis implements Cache
 	 */
 	public function HLen(string $key)
     {
-        $isRetried = false;
-        START:
-        try
-        {
-            $result = $this->_conn->hLen( $key );
-        }
-        catch(\RedisException $exception)
-        {
-            if( !$isRetried )
-            {
-                $this->_handleException($exception, __FUNCTION__);
-                $isRetried = true;
-                goto START;
-            }
-            return false;
-        }
-        return $result;
+        return $this->_exec('hLen', $key);
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function HExists( string $key) : bool
+    {
+        return $this->_exec('hExists', $key);
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function HDel( string $key) : bool
+    {
+        return $this->_exec('hDel', $key);
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function HIncrBy( string $key, int $step)
+    {
+        return $this->_exec('hIncrBy', $key, $step);
+    }
+
+    /**
+     * @param string $key
+     * @return bool|array
+     */
+    public function HKeys( string $key)
+    {
+        return $this->_exec('hKeys', $key);
+    }
+
+    /**
+     * @param string $key
+     * @return bool|array
+     */
+    public function HVals( string $key)
+    {
+        return $this->_exec('hVals', $key);
+    }
+
+    /**
+     * @param string $key
+     * @return bool|array
+     */
+    public function HGetAll( string $key)
+    {
+        return $this->_exec('hGetAll', $key);
+    }
+
+
+    /**
+     * @param string $key
+     * @return bool|int
+     */
+    public function Decr( string $key)
+    {
+        return $this->_exec('decr', $key);
+    }
+
+    /**
+     * @param string $key
+     * @param int    $step
+     * @return bool|int
+     */
+    public function DecrBy( string $key, int $step)
+    {
+        return $this->_exec('decrBy', $key, $step);
+    }
+
+
+    /**
+     * @param string $key
+     * @return bool|int
+     */
+    public function Incr( string $key)
+    {
+        return $this->_exec('incr', $key);
+    }
+
+    /**
+     * @param string $key
+     * @param int    $step
+     * @return bool|int
+     */
+    public function IncrBy( string $key, int $step)
+    {
+        return $this->_exec('incrBy', $key, $step);
+    }
+
+
+    /**
+     * @param string $channel
+     * @param string $msg
+     * @return mixed
+     */
+    public function Publish( string $channel, string $msg)
+    {
+        return $this->_exec('publish', $channel, $msg);
+    }
 
 	/**
 	 * Ping
@@ -420,7 +430,32 @@ class Redis implements Cache
 	 */
 	public function Ping()
     {
-        return $this->_conn->ping ();
+        return $this->_exec('ping', '');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function DbSize()
+    {
+        return $this->_exec('dbSize', '');
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function FlushDB()
+    {
+        return $this->_exec('flushDB', '');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function FlushAll()
+    {
+        return $this->_exec('flushAll', '');
     }
 
 
@@ -428,9 +463,120 @@ class Redis implements Cache
 	 * close 关闭连接
 	 * @return void
 	 */
-	public function close()
+	public function Close()
     {
         $this->_conn->close();
+    }
+
+
+    /**
+     * @param string $function
+     * @param string $key
+     * @param string ...$values
+     * @return mixed
+     */
+    private function _exec( string $function, string $key, string ...$values)
+    {
+        $isRetried = false;
+        START:
+        try
+        {
+            switch ($function)
+            {
+                case 'dbSize':
+                case 'flushDB':
+                case 'flushAll':
+                case 'ping':     //done
+                    $result = $this->_conn->$function();
+                    break;
+                case 'get':     //done
+                case 'select':   //done
+                case 'watch':    //done
+                case 'decr':     //done
+                case 'incr':     //done
+                case 'hKeys':    //done
+                case 'hGetAll':  //done
+                case 'hVals':    //done
+                case 'hLen':     //done
+                    $result = $this->_conn->$function($key);
+                    break;
+                case 'set':  //done
+                    $result = $this->_conn->$function($key, $values[0], 1);
+                    break;
+                case 'lPush':  //done
+                case 'rPush':  //done
+                    $result = $this->_conn->$function( $key, $values[0] );
+                    break;
+                case 'rPop':   //done
+                case 'lPop':   //done
+                case 'brPop':  //done
+                case 'blPop':  //done
+                    $result = $this->_conn->$function( $key, (int)$values[0] );
+                    break;
+                case 'hSet':    //done
+                case 'hSetNx':  //done
+                    $result = $this->_conn->$function( $key, $values[0], $values[1]);
+                    break;
+                case 'hGet':     //done
+                case 'publish':  //done
+                case 'hExists':  //done
+                case 'rPushx':
+                case 'append':   //done
+                case 'setnx':    //done
+                    $result = $this->_conn->$function( $key, $values[0]);
+                    break;
+                case 'decrBy':  //done
+                case 'incrBy':  //done
+                    $result = $this->_conn->$function( $key, (int)$values[0]);
+                    break;
+                case 'hDel':   //done
+                case 'hMGet':  //单独处理
+                    $result = $this->_conn->$function($key, $values);
+                    break;
+                case 'hMSet': //单独处理
+                    $result = $this->_conn->$function($key, $values);
+                    break;
+                case 'hIncrBy':  //done
+                    $result = $this->_conn->$function($key, $values[0], (int)$values[1]);
+                    break;
+                case 'setex':  //done
+                    $result = $this->_conn->multi($key, (int)$values[1], $values[0]);
+                    break;
+                case 'multi':    //done
+                    $result = $this->_conn->$function( (int)$key );
+                    break;
+                default:
+                    return false;
+            }
+        }
+        catch(\RedisException $exception)
+        {
+            if( !$isRetried )
+            {
+                $this->_handleException($exception, $function);
+                $isRetried = true;
+                goto START;
+            }
+            return false;
+        }
+        return $result;
+    }
+
+    /**
+     * @param \RedisException $exception
+     * @param string          $function
+     */
+    private function _handleException( \RedisException $exception, string $function)
+    {
+        Log::Warning(__CLASS__.'::'.$function." failed, ".$exception->getMessage(), self::LOG_NAME);
+        if(
+            false!==strpos($exception->getMessage(), 'server went away') ||
+            false!==strpos($exception->getMessage(), 'Connection lost')
+        )
+        {
+            Log::Warning('Trying to reconnect.', self::LOG_NAME);
+            $this->InitConnection();
+        }
     }
 
 }
