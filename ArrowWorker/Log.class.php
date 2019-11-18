94629f29fce9a90ebd750942e1c9c8f415af89db
louis <louis531029167@163.com>
2019-11-18 22:33:11 +0800

    optimize log buffer

--- ArrowWorker/Log.class.php
+++ ArrowWorker/Log.class.php
@@ -515,7 +515,7 @@ class Log
         $filePath = $fileDir . $fileExt;
         if ( !is_dir( $fileDir ) )
         {
-            if ( !mkdir( $fileDir, 0777, true ) )
+            if ( !mkdir( $fileDir, 0666, true ) )
             {
                 Log::Dump( self::LOG_PREFIX . " [ EMERGENCY ] make log directory:{$fileDir} failed . " );
                 return false;
