d8563d15a979be17471f0a06b83bdeeb7334d8ba
louis <louis531029167@163.com>
2019-11-18 19:55:05 +0800

    optimize log buffer

--- ArrowWorker/Log.class.php
+++ ArrowWorker/Log.class.php
@@ -580,7 +580,7 @@ class Log
     private function _initCoroutine()
     {
         Coroutine::Enable();
-        for ( $i = 0; $i < 50; $i++ )
+        for ( $i = 0; $i < 20; $i++ )
         {
             Coroutine::Create( function ()
             {
