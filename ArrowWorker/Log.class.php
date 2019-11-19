cdc72476480b45bd0eb1d4f5621e11125d3c5146
louis <louis531029167@163.com>
2019-11-19 17:13:09 +0800

    optimize log module

--- ArrowWorker/Log.class.php
+++ ArrowWorker/Log.class.php
@@ -590,7 +590,7 @@ class Log
             } );
         }
 
-        for ( $i = 0; $i < 2; $i++ )
+        for ( $i = 0; $i < 8; $i++ )
         {
             Coroutine::Create( function ()
             {
