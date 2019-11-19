406b8143c14f6fc9f360efdc8625c3a9f5b84525
louis <louis531029167@163.com>
2019-11-19 17:18:13 +0800

    optimize log module

--- ArrowWorker/Log.class.php
+++ ArrowWorker/Log.class.php
@@ -590,7 +590,7 @@ class Log
             } );
         }
 
-        for ( $i = 0; $i < 8; $i++ )
+        for ( $i = 0; $i < 16; $i++ )
         {
             Coroutine::Create( function ()
             {
