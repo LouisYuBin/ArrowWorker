c2ca463c3211b2f955cf9595b9ae4d1dc64dc276
于彬 <yubin@vchangyi.com>
2020-06-26 12:18:02 +0800

    finish optimize log

---
+++ ArrowWorker/Library/Http.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * By yubin at 2020-03-24 10:41.
+ */
+
+namespace ArrowWorker\Library;
+
+
+class Http
+{
+
+    const METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'];
+
+}
